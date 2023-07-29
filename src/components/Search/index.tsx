a31941371d951467fea3c400018dfa3678d02bf3
Ikaro Vieira <ikarovieiraa@gmail.com>
2023-07-29 01:50:14 +0000

    search

---
+++ src/components/Search/index.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image'
+
+async function Search() {
+  // const mangas = await fetch(
+  //   'https://scanlattor-site-back.bohr.io/api/mangas',
+  //   {
+      
+  //   }
+  // ).then((res) => res.json())
+  // console.log(mangas)
+
+  return (
+    <div>
+      <div className="bg-quaternary bg-opacity-50 rounded-full p-1">
+        <Image
+          src="/search-icon.svg"
+          alt="Search icon"
+          width={26}
+          height={26}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Search
