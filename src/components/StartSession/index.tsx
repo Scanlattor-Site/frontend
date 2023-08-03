0f51c5e3e41ad11c726a7da8cfff20ced0ed6524
Ikaro Vieira <ikarovieiraa@gmail.com>
2023-08-03 03:16:43 +0000

    💄 style: aumenta o gap

--- src/components/StartSession/index.tsx
+++ src/components/StartSession/index.tsx
@@ -11,8 +11,8 @@ export default function StartSession() {
 
   return (
     <section className="bg-white rounded-2xl md:absolute md:-top-8 max-w-[40rem] w-full mt-8 md:mt-0 shadow-md">
-      <div className="flex flex-col md:flex-row items-center justify-between py-4 px-5 gap-2">
-        <h3 className="text-sm text-primary font-semibold">
+      <div className="flex flex-col md:flex-row items-center justify-between py-4 px-5 gap-3">
+        <h3 className="text-sm text-primary font-semibold text-center">
           Faça login para ficar por dentro de todo o conteúdo
         </h3>
         <Link href="/login">
